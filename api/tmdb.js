0f03883332f9d093914c1bb73306f1cdd49f1952
DonMaderos <alexneitor5@gmail.com>
2026-07-10 16:28:59 +0200

    Actualización del TMDB...

--- api/tmdb.js
+++ api/tmdb.js
@@ -296,7 +296,6 @@ export default async function handler(req, res) {
 
                 try {
                     const detailRes = await fetch(`${baseUrl}/${tipo}/${queryTrimmed}?append_to_response=watch/providers,release_dates,content_ratings&language=${tmdbLang}`, { headers });
-                    const detailRes = await fetch(`${baseUrl}/${tipo}/${queryTrimmed}?append_to_response=watch/providers,release_dates,content_ratings&language=${tmdbLang}`, { headers });
 
                     if (!detailRes.ok) return res.status(200).json([]);
 
